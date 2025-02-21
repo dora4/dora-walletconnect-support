41022bbd28d9b274259a16a3537bb40d8b9624d3
dora <dora924666990@gmail.com>
2025-02-22 05:26:18 +0800

    增加底层日志打印

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -65,7 +65,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora-walletconnect-support"
-                version = "1.5"
+                version = "1.6"
             }
         }
     }
