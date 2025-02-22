ff2104e5c024c4dbc97583e7f09f6224e99a917e
dora <dora924666990@gmail.com>
2025-02-22 23:52:04 +0800

    优化底层代码

--- lib/src/main/java/dora/trade/DoraTrade.kt
+++ lib/src/main/java/dora/trade/DoraTrade.kt
@@ -5,8 +5,6 @@ import android.content.Context
 import android.graphics.Color
 import android.util.Log
 import com.walletconnect.android.Core
-import com.walletconnect.android.CoreClient
-import com.walletconnect.android.relay.ConnectionType
 import com.walletconnect.web3.modal.client.Modal
 import com.walletconnect.web3.modal.client.Web3Modal
 import com.walletconnect.web3.modal.client.models.request.SentRequestResult
@@ -15,7 +13,6 @@ import dora.trade.activity.WalletConnectActivity
 import dora.util.IntentUtils
 import dora.util.ToastUtils
 import dora.widget.DoraAlertDialog
-
 
 /**
  * https://dorafund.com
@@ -38,7 +35,7 @@ object DoraTrade {
         listener: PayListener? = null
     ) {
         System.loadLibrary("pay-core")
-        initWalletConnect(application, appName, appDesc, domainUrl, supportChains)
+        appMetaData = nativeInitWalletConnect(application, appName, appDesc, domainUrl, supportChains)
         listener?.let { setPayListener(it) }
     }
 
@@ -48,56 +45,16 @@ object DoraTrade {
         initPayListener()
     }
 
+    /**
+     * 初始化钱包连接配置。
+     */
     private external fun nativeInitWalletConnect(
         application: Application,
         appName: String,
         appDesc: String,
         domainUrl: String,
         supportChains: Array<Modal.Model.Chain>
-    ): Array<String>
-
-    /**
-     * 初始化钱包连接配置。
-     */
-    private fun initWalletConnect(
-        application: Application,
-        appName: String,
-        appDesc: String,
-        domainUrl: String,
-        supportChains: Array<Modal.Model.Chain>
-    ){
-        val (relayUrl, serverUri, icon, redirect) = nativeInitWalletConnect(application, appName, appDesc, domainUrl, supportChains)
-        appMetaData = Core.Model.AppMetaData(
-            name = appName,
-            description = appDesc,
-            url = domainUrl,
-            icons = listOf(icon),
-            redirect = redirect
-        )
-        CoreClient.initialize(
-            relayServerUrl = serverUri,
-            connectionType = ConnectionType.AUTOMATIC,
-            application = application,
-            metaData = appMetaData,
-        ) {
-        }
-        Web3Modal.initialize(
-            Modal.Params.Init(core = CoreClient,
-                recommendedWalletsIds = listOf(
-                    "c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a",//Uniswap
-                    "1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369",//Rainbow
-                    "20459438007b75f4f4acb98bf29aa3b800550309646d375da5fd4aac6c2a2c66",//TokenPocket
-                    "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",//TrustWallet
-                    "ef333840daf915aafdc4a004525502d6d49d77bd9c65e0642dbaefb3c2893bef",//ImToken
-                    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",//MetaMask
-                    "19177a98252e07ddfc9af2083ba8e07ef627cb6103467ffebb3f8f4205fd7927",//Ledger
-                )
-            ),
-            onSuccess = {},
-            onError = { error -> }
-        )
-        Web3Modal.setChains(supportChains.asList())
-    }
+    ) : Core.Model.AppMetaData
 
     /**
      * 初始化支付监听器。
