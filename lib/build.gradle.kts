1e10b0a90d0fd2598a5514db18e3aa696f167430
dora <dora924666990@gmail.com>
2025-02-22 18:18:45 +0800

    修复底层崩溃

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -65,7 +65,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora-walletconnect-support"
-                version = "1.9"
+                version = "1.10"
             }
         }
     }
