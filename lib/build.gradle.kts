910fc3c554eb61a15627e022493bd935cbada88f
dora <dora924666990@gmail.com>
2025-04-02 02:39:35 +0800

    Fix bugs in the Native layer.(https://github.com/dora4/dora-walletconnect-support/issues/5)

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -67,7 +67,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora-walletconnect-support"
-                version = "1.59"
+                version = "1.60"
             }
         }
     }
