1c8259fb95f15529cb453466a8a91cc61f84796a
dora <dora924666990@gmail.com>
2025-05-29 13:49:48 +0800

    Optimize the native layer code.(https://github.com/dora4/dora-walletconnect-support/issues/10)

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -73,7 +73,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora-walletconnect-support"
-                version = "1.132"
+                version = "1.133"
             }
         }
     }
