5984c73aa3d1ecce7415ba25d5e1c734353d1094
dora <dora924666990@gmail.com>
2025-06-01 16:59:05 +0800

    SDK layer requests permission to bind to a VPN.(https://github.com/dora4/dora-walletconnect-support/issues/10)

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -74,7 +74,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora-walletconnect-support"
-                version = "1.157"
+                version = "1.158"
             }
         }
     }
