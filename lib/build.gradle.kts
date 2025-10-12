9a41b7af99d10f33840046f41692f69851c4bb0f
dora <dora924666990@gmail.com>
2025-10-13 02:05:58 +0800

    Added ERC20 transfer.(https://github.com/dora4/dora-walletconnect-support/issues/11)

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -78,7 +78,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora-walletconnect-support"
-                version = "2.1.7"
+                version = "2.1.8"
             }
         }
     }
