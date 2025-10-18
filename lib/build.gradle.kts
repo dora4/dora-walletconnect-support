ef3212896c3c2b34b7b7ce902f6dc06b5d209f57
dora <dora924666990@gmail.com>
2025-10-18 23:31:12 +0800

    Detect whether the node supports EIP-1559.(https://github.com/dora4/dora-walletconnect-support/issues/11)

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -78,7 +78,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora-walletconnect-support"
-                version = "2.1.23"
+                version = "2.1.24"
             }
         }
     }
